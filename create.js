0567ab02fbaeae4a096d108a732b4abd203a0170
Renato Mertz <renatomertz@gmail.com>
2020-03-07 17:32:54 -0300

    create async FIX

--- create.js
+++ create.js
@@ -4,7 +4,7 @@ import { success, failure } from "./libs/response-lib";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-export function main(event, context, callback) {
+export async function main(event, context, callback) {
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
